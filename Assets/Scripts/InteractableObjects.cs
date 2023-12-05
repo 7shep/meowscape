fb181ffe11d3467490806ef4a465e125e3304f40
Alex <sheppenguin@gmail.com>
2023-12-05 10:17:03 -0500

    Update InteractableObjects.cs

--- Assets/Scripts/InteractableObjects.cs
+++ Assets/Scripts/InteractableObjects.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class InteractableObject : MonoBehaviour
 {
@@ -12,6 +14,49 @@ public class InteractableObject : MonoBehaviour
     }
 
     public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+
+    void Update()
+    {
+        if (isFocus)    // If currently being focused
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            // If we haven't already interacted and the player is close enough
+            if (!hasInteracted && distance <= radius)
+            {
+                // Interact with the object
+                hasInteracted = true;
+                Interact();
+            }
+        }
+    }
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        hasInteracted = false;
+        player = playerTransform;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        hasInteracted = false;
+        player = null;
+    }
+
+    // This method is meant to be overwritten
+    public virtual void Interact()
+    {
+
+    }
+
+
 
     void OnDrawGizmosSelected()
     {
