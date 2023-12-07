a6c7855b8348cb420c8abdf8ae49b6580d567955
Trenton Scott <twscott06@gmail.com>
2023-12-07 09:59:32 -0500

    pause menu fixed

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-
     public GameObject pauseMenuUI;
+    public PlayerMovementAdvanced playerMovement; // Reference to the PlayerMovementAdvanced script
 
-    public void Start()
+    private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor at the beginning
+        Cursor.visible = false;
         pauseMenuUI.SetActive(false);
     }
 
@@ -20,7 +22,7 @@ public class PauseMenu : MonoBehaviour
             if (GameIsPaused)
             {
                 Resume();
-            } 
+            }
             else
             {
                 Pause();
@@ -28,21 +30,24 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    public void Resume ()
+    public void Resume()
     {
         Debug.Log("resumed");
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor
+        Cursor.visible = false;
+        playerMovement.enabled = true; // Enable the PlayerMovementAdvanced script
     }
 
     public void Pause()
     {
+        Cursor.lockState = CursorLockMode.None; // Unlock the cursor
         Cursor.visible = true;
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        playerMovement.enabled = false; // Disable the PlayerMovementAdvanced script
     }
-
-    
 }
